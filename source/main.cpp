2ea1d44fbd5b113b537859b3170a017e12c796eb
Leroy Ketelaars <leroy.ketelaars@truelime.nl>
2020-02-08 02:36:49 +0100

    updated log function to be able to pause after a log statement, loading dns entries from config works now, initializing BBA hardware and FS is now functional, but resolving DNS does not work

--- source/main.cpp
+++ source/main.cpp
@@ -12,14 +12,19 @@
 #include <ogcsys.h>
 #include <gccore.h>
 #include <errno.h>
-#include <array>
 
 #include "file_browse.h"
 #include "run_dol.h"
 #include "input.h"
 #include "dns.h"
-
+#define SCREEN_COLS 70
+#define ENTRIES_PER_SCREEN 22
+#define ENTRIES_START_ROW 5
+#define ENTRIES_START_COLUMN 5
+#define ENTRY_PAGE_LENGTH 20
 #define MAXPATHLEN 512
+#define ONE_MEGABYTE 1048576
+#define FILE_DOWNLOAD_BUFFER_SIZE (ONE_MEGABYTE * 4)
 
 static void* xfb = NULL;
 
@@ -30,6 +35,9 @@ FILE* logFileHandle = nullptr;
 #define LOGBUFFERSIZE 10
 std::string logbuffer[LOGBUFFERSIZE];
 int logEntries = 0;
+
+void WaitForAButton();
+void writeBufferToFile(char* buffer, size_t bufferSize, std::string destinationPath);
 
 std::string getWorkingPath() {
 	char cwdbuff[1024];
@@ -58,16 +66,45 @@ void log_flush() {
 		logEntries = 0;
 	}
 }
-void log(std::string message, bool directWriteMode = false) {
-	if ((logEntries % LOGBUFFERSIZE) == (LOGBUFFERSIZE - 1) || directWriteMode) {
+//int countOccurrencesInString(std::string s, char charToLookFor) {
+//	int count = 0;
+//	for (int i = 0; i < s.size(); i++)
+//		if (s[i] == charToLookFor) ++count;
+//	return count;
+//}
+void log(std::string message, bool pause = false) {
+	/*if ((logEntries % LOGBUFFERSIZE) == (LOGBUFFERSIZE - 1) || directWriteMode) {
 		log_flush();
-	}
-
+	}*/
+	
 	time_t now = time(0);
 
 	// convert now to string form
 	std::string timestring(ctime(&now));
-	logbuffer[logEntries++ % LOGBUFFERSIZE] = std::move(timestring.substr(0, timestring.length() - 1) + " | " + message);
+	logbuffer[logEntries++ % LOGBUFFERSIZE] = std::move(timestring.substr(0, timestring.length() - 1) + " | " + message + '\0');
+	logbuffer[logEntries+1 % LOGBUFFERSIZE] = "" + '\0';
+
+	std::printf("\x1b[2J");
+	int i = 0;
+	for (auto entry : logbuffer) {
+		if (entry.length() < 1) continue;
+		std::printf("\x1b[%d;%dH", i + ENTRIES_START_ROW, ENTRIES_START_COLUMN);
+		std::printf("%s", entry.c_str());
+		++i;
+		//fwrite(m.c_str(), sizeof(char), sizeof(char) * m.length(), logFileHandle);
+	}
+	if (pause) WaitForAButton();
+}
+
+void WaitForAButton() {
+	while (1) {
+		VIDEO_WaitVSync();
+		INPUT_Scan();
+
+		if (INPUT_ButtonsDown() & PAD_BUTTON_A) {
+			return;
+		}
+	}
 }
 
 void Stop() {
@@ -78,23 +115,14 @@ void Stop() {
 
 		int buttonsDown = INPUT_ButtonsDown();
 
-		if (buttonsDown & PAD_BUTTON_START) {
-			exit(0);
-		}
-
 		if (buttonsDown & PAD_TRIGGER_Z) {
 			break;
 		}
 	}
 }
-void touchFile(std::string fullPathFilename) {
-	FILE* filehandle = fopen(fullPathFilename.c_str(), "b+");
-	fwrite("123", sizeof(char), sizeof(char) * 3, filehandle);
-	fclose(filehandle);
-}
-std::string getHostname(std::string url) {
-	const auto domainStart = url.find_first_of('/');
-	const auto domainEnd = url.substr(domainStart).find_first_of('/');
+std::string extractHostnameFromString(std::string url) {
+	const auto domainStart = url.find_first_of(':') + 3;
+	const auto domainEnd = url.find_first_of('/', domainStart + 1);
 	return url.substr(domainStart, domainEnd - domainStart);
 }
 void bbaConfig() {
@@ -103,95 +131,99 @@ void bbaConfig() {
 	char netmask[16] = { 0 };
 	s32 ret = if_config(localip, netmask, gateway, TRUE, 20);
 	if (ret >= 0) {
-		//printf("bba setup: ip: %s, gw: %s, mask %s\n", localip, gateway, netmask);
-		log("bba setup success!");
+		log("bba setup success! \nip: [" + std::string(localip) + "]\ngateway: [" + std::string(gateway) + "]\nnetmask: [" + std::string(netmask) + "]");
 	}
 	else {
-		log("bba setup failed");
+		log("bba setup failed", true);
 	}
-	log("setting dns servers from config..", true);
-	pm_getDnsServers((getWorkingPath() + "/resolv.conf").c_str());
+	auto configPath = (getWorkingPath() + "/resolv.conf");
+	log("setting dns servers from config [" + configPath + "]");
+	pm_getDnsServers(configPath.c_str());
 	for (auto dnsEntry : dns_servers) {
-		log(std::string("DNS: ") + dnsEntry);
+		auto dns = std::string(dnsEntry);
+		if(!dns.empty()) log(dns);
 	}
+	//strcpy(dns_servers[0], "\0");
 	log("done", true);
 }
 void printAddress(sockaddr_in address) {
 	log("Addr family: ");
 	switch (address.sin_family) {
-	case AF_INET: log("TCPIP version 4"); break;
+	case AF_INET: log("[IP version 4]"); break;
 	default:
 		log(" - UNSPECIFIED");
 			break;
 	}
 	char ip[30];
 	strcpy(ip, (char*)inet_ntoa((struct in_addr)address.sin_addr));
-	log(std::string("Addr ip: ") + ip);
-	log(std::string("Addr port: ") + std::to_string(address.sin_port));
+	log(std::string("Addr ip: [") + ip + "]");
+	log(std::string("Addr port: [") + std::to_string(address.sin_port) + "]", true);
 }
 //the headers used have been tested using firefox and work for making the request
 int downloadFile(std::string apiEndpoint, std::string filename) {
+	log("downloadFile", true);
 	int sock_descriptor; // integer number to access socket
 	log("endpoint dns lookup...", true);
-	struct sockaddr_in serv_addr = pm_gethostbyname((unsigned char*)getHostname(apiEndpoint).c_str(), T_IPv4ADDRESS);
-	log("got address", true);
-	//struct hostent* server; // from netdb.h to determine host name out of ip address
-	char recvBuff[1024];  // Receiving buffer
-	std::string header = "GET " + apiEndpoint + " HTTP/1.1"
-		"User-Agent: PuppetMaster/0.1 (Nintendo Gamecube; PPC32)\n"
-		"Accept-Encoding: gzip, deflate, br\n"
-		"Accept-Language: en-GB,en;q=0.8,en-US;q=0.6,da;q=0.4\n"
-		"Connection: keep-alive\n"
-		"Upgrade-Insecure-Requests: 1\n"
-		"Pragma: no-cache\n"
-		"Cache - Control : no - cache\n"
-		"Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\n\n";
+	auto remoteHostname = extractHostnameFromString(apiEndpoint);
+	log("remote hostname: " + remoteHostname, true);
+	auto serv_addr = pm_gethostbyname((unsigned char*)remoteHostname.c_str(), T_IPv4ADDRESS);
+	printAddress(serv_addr);
 
-	sock_descriptor = net_socket(AF_INET, SOCK_STREAM, IPPROTO_IP); // SOCK_STREAM = TCP, AF_INET = DOMAIN
-	if (sock_descriptor == INVALID_SOCKET) {
-		log("Failed creating socket", true);
-	}
-	else {
-		log("Socket initialized", true);
-	}
-	/*memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(80);
-	serv_addr.sin_addr.s_addr = inet_addr("172.217.17.86");
-	log("target ip: 172.217.17.86");*/
-	/*server = net_
-	if (server == nullptr) {
-		return 0;
-	}*/
-	//memcpy((char*)&(serv_addr.sin_addr.s_addr), (char*)(server->h_addr), server->h_length);
+	//char recvBuff[FILE_DOWNLOAD_BUFFER_SIZE];  // Receiving buffer
+	//std::string header = "GET " + apiEndpoint + " HTTP/1.1"
+	//	"User-Agent: PuppetMaster/0.1 (Nintendo Gamecube; PPC32)\n"
+	//	"Accept-Encoding: gzip, deflate, br\n"
+	//	"Accept-Language: en-GB,en;q=0.8,en-US;q=0.6,da;q=0.4\n"
+	//	"Connection: keep-alive\n"
+	//	"Upgrade-Insecure-Requests: 1\n"
+	//	"Pragma: no-cache\n"
+	//	"Cache - Control : no - cache\n"
+	//	"Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\n\n";
 
-	if (net_connect(sock_descriptor, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) < 0) {
-		log("failed to connect to server");
-	}
-	else {
-		log("socket connected! ");
-		net_write(sock_descriptor, header.c_str(), sizeof(header.c_str()));
+	//sock_descriptor = net_socket(AF_INET, SOCK_STREAM, IPPROTO_IP); // SOCK_STREAM = TCP, AF_INET = DOMAIN
+	//log("net_socket", true);
 
-		auto fileDownloadPath = (getWorkingPath() + std::string("/pmstore/") + filename);
-		log("file download to: " + fileDownloadPath);
-		FILE* filehandle = fopen(fileDownloadPath.c_str(), "wb+");
+	//if (sock_descriptor == INVALID_SOCKET) {
+	//	log("Failed creating socket", true);
+	//}
+	//else {
+	//	log("Socket initialized", true);
+	//}
 
-		int readBytes = 0;
-		while (true) {
-			memset(recvBuff, 0, 1024);
-			if ((readBytes = read(sock_descriptor, recvBuff, sizeof(recvBuff) - 1)) > 0) {
-				fwrite(recvBuff, sizeof(recvBuff), 1, filehandle);
-			}
-			else break;
-		}
-		std::fclose(filehandle);
+	//if (net_connect(sock_descriptor, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) < 0) {
+	//	log("failed to connect to server", true);
+	//}
+	//else {
+	//	log("socket connected! downloading file..", true);
 
-		log("download finished!");
-	}
+	//	net_write(sock_descriptor, header.c_str(), sizeof(header.c_str()));
+
+	//	int readBytes = 0;
+	//	while (true) {
+	//		memset(recvBuff, 0, FILE_DOWNLOAD_BUFFER_SIZE);
+	//		if ((readBytes = net_read(sock_descriptor, recvBuff, sizeof(recvBuff) - 1)) < 1) {
+	//			break;
+	//		}
+	//	}
+	//	net_close(sock_descriptor);
+	//	log("download complete!", true);
+
+	//	auto fileDownloadPath = (getWorkingPath() + std::string("/pmstore/") + filename);
+	//	writeBufferToFile(recvBuff, readBytes, fileDownloadPath);
+	//}
 	
 	return 0;
 }
 
+void writeBufferToFile(char* buffer, size_t bufferSize, std::string destinationPath) {
+	log("opened file", true);
+	FILE* filehandle = fopen(destinationPath.c_str(), "wb+");
+	log("writing data...", true);
+	fwrite(buffer, bufferSize, 1, filehandle);
+	log("done!", true);
+	fclose(filehandle);
+	filehandle = nullptr;
+}
 int main() {
 
 	std::string filename;
@@ -205,7 +237,6 @@ int main() {
 	INPUT_Init();
 
 	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	bbaConfig();
 
 	VIDEO_Configure(rmode);
 
@@ -219,95 +250,96 @@ int main() {
 	console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * 2);
 	setvbuf(stdout, NULL, _IONBF, 0);
 
-
 	log("Initialising FS...");
 	if (fatInitDefault()) {
-		log("FS OK!", true);
+		log("FS OK!");
 	}
 	else {
 		log("FS FAIL!", true);
 		Stop();
 	}
+	bbaConfig();
 	std::vector<std::string> extensionList;
 	extensionList.push_back(".dol");
 	extensionList.push_back(".argv");
 	bool doOnce = false;
 	log("setup complete", true);
-	while (1) {
-		std::string url = "https://i.ytimg.com/vi/HevHbYbnLHQ/hqdefault.jpg";
-		downloadFile(url, "test.jpg");
-		log("displaying file browser", true);
-		filename = browseForFile(extensionList);
-		log("get cwd");
-		// Construct a command line
-		getcwd(filePath, MAXPATHLEN);
-		pathLen = strlen(filePath);
-		std::vector<char*> argarray;
+	std::string url = "https://i.ytimg.com/vi/HevHbYbnLHQ/hqdefault.jpg";
+	log("attempting download from url: [" + url + "]");
+	downloadFile(url, "test.jpg");
+	//while (1) {
+	//	log("displaying file browser", true);
+	//	filename = browseForFile(extensionList);
+	//	log("get cwd");
+	//	// Construct a command line
+	//	getcwd(filePath, MAXPATHLEN);
+	//	pathLen = strlen(filePath);
+	//	std::vector<char*> argarray;
 
-		if (strcasecmp(filename.c_str() + filename.size() - 5, ".argv") == 0) {
+	//	if (strcasecmp(filename.c_str() + filename.size() - 5, ".argv") == 0) {
 
-			FILE* argfile = std::fopen(filename.c_str(), "rb");
-			char str[MAXPATHLEN], * pstr;
-			const char seps[] = "\n\r\t ";
+	//		FILE* argfile = std::fopen(filename.c_str(), "rb");
+	//		char str[MAXPATHLEN], * pstr;
+	//		const char seps[] = "\n\r\t ";
 
-			while (fgets(str, MAXPATHLEN, argfile)) {
-				// Find comment and end string there
-				if ((pstr = strchr(str, '#')))
-					*pstr = '\0';
+	//		while (fgets(str, MAXPATHLEN, argfile)) {
+	//			// Find comment and end string there
+	//			if ((pstr = strchr(str, '#')))
+	//				*pstr = '\0';
 
-				// Tokenize arguments
-				pstr = strtok(str, seps);
+	//			// Tokenize arguments
+	//			pstr = strtok(str, seps);
 
-				while (pstr != NULL) {
-					argarray.push_back(strdup(pstr));
-					pstr = strtok(NULL, seps);
-				}
-			}
-			std::fclose(argfile);
-			filename = argarray.at(0);
-		}
-		else {
-			argarray.push_back(strdup(filename.c_str()));
-		}
+	//			while (pstr != NULL) {
+	//				argarray.push_back(strdup(pstr));
+	//				pstr = strtok(NULL, seps);
+	//			}
+	//		}
+	//		std::fclose(argfile);
+	//		filename = argarray.at(0);
+	//	}
+	//	else {
+	//		argarray.push_back(strdup(filename.c_str()));
+	//	}
 
-		if (strcasecmp(filename.c_str() + filename.size() - 4, ".dol") != 0 || argarray.size() == 0) {
-			iprintf("no dol file specified\n");
-			log("no dol file specified");
-		}
-		else {
-			char* name = argarray.at(0);
-			strcpy(filePath + pathLen, name);
-			free(argarray.at(0));
-			argarray.at(0) = filePath;
-			char formatted[2048];
-			sprintf(formatted, "Running %s with %d parameters\n", argarray[0], argarray.size());
-			log(formatted);
-			int err = runDOL(argarray[0], argarray.size(), (const char**)&argarray[0]);
-			memset(formatted, 0, sizeof(char) * 2048);
-			sprintf(formatted, "Start failed. Error %d\n", err);
-			log(formatted);
-		}
+	//	if (strcasecmp(filename.c_str() + filename.size() - 4, ".dol") != 0 || argarray.size() == 0) {
+	//		iprintf("no dol file specified\n");
+	//		log("no dol file specified");
+	//	}
+	//	else {
+	//		char* name = argarray.at(0);
+	//		strcpy(filePath + pathLen, name);
+	//		free(argarray.at(0));
+	//		argarray.at(0) = filePath;
+	//		char formatted[2048];
+	//		sprintf(formatted, "Running %s with %d parameters\n", argarray[0], argarray.size());
+	//		log(formatted);
+	//		int err = runDOL(argarray[0], argarray.size(), (const char**)&argarray[0]);
+	//		memset(formatted, 0, sizeof(char) * 2048);
+	//		sprintf(formatted, "Start failed. Error %d\n", err);
+	//		log(formatted);
+	//	}
 
-		while (argarray.size() != 0) {
-			free(argarray.at(0));
-			argarray.erase(argarray.begin());
-		}
-		u32 held;
-		held = INPUT_ButtonsHeld();
-		if (held & PAD_BUTTON_X) {
-			if (!doOnce) {
-				log("hold X, download file call", true);
-				doOnce = true;
-				std::string url = "https://i.ytimg.com/vi/HevHbYbnLHQ/hqdefault.jpg";
-				downloadFile(url, "test.jpg");
-			}
-		}
-		do {
-			INPUT_Scan();
+	//	while (argarray.size() != 0) {
+	//		free(argarray.at(0));
+	//		argarray.erase(argarray.begin());
+	//	}
+	//	u32 held;
+	//	held = INPUT_ButtonsHeld();
+	//	if (held & PAD_BUTTON_X) {
+	//		if (!doOnce) {
+	//			log("hold X, download file call", true);
+	//			doOnce = true;
+	//			std::string url = "https://i.ytimg.com/vi/HevHbYbnLHQ/hqdefault.jpg";
+	//			downloadFile(url, "test.jpg");
+	//		}
+	//	}
+	//	do {
+	//		INPUT_Scan();
 
-			VIDEO_WaitVSync();
-		} while (held & PAD_BUTTON_START);
+	//		VIDEO_WaitVSync();
+	//	} while (held & PAD_BUTTON_START);
 
-	}
+	//}
 	exitLog();
 }
